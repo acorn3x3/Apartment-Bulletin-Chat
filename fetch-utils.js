0d19cfaafabc60dbe22751159f3c644e9dca7d3d
Emily Sellers <emilymelindasellers@gmail.com>
2022-10-11 18:03:03 -0700

    add post editor form event handlers and createPost in fetch-utils

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,29 +1,32 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
-const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+// const SUPABASE_URL = '';
+// const SUPABASE_KEY = '';
+// const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
-/* Auth related functions */
+// /* Auth related functions */
 
-export function getUser() {
-    return client.auth.user();
-}
+// export function getUser() {
+//     return client.auth.user();
+// }
 
-export async function signUpUser(email, password) {
-    return await client.auth.signUp({
-        email,
-        password,
-    });
-}
+// export async function signUpUser(email, password) {
+//     return await client.auth.signUp({
+//         email,
+//         password,
+//     });
+// }
 
-export async function signInUser(email, password) {
-    return await client.auth.signIn({
-        email,
-        password,
-    });
-}
+// export async function signInUser(email, password) {
+//     return await client.auth.signIn({
+//         email,
+//         password,
+//     });
+// }
 
-export async function signOutUser() {
-    return await client.auth.signOut();
-}
+// export async function signOutUser() {
+//     return await client.auth.signOut();
+// }
 
 /* Data functions */
+export async function createPost(newPost) {
+    console.log(newPost);
+}
--- post-editor/post-editor.js
+++ post-editor/post-editor.js
@@ -1,1 +1,22 @@
+// Imports
+import { createPost } from '/fetch-utils.js';
 
+// Get DOM
+const postForm = document.getElementById('post-form');
+
+// State
+let error = null;
+
+// Events
+postForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const formData = new FormData(postForm);
+
+    const newPost = {
+        title: formData.get('post-title'),
+        content: formData.get('post-content'),
+    };
+    createPost(newPost);
+});
+
+// Display
