52feba52d1798c89d76081e1ab9ddd59e59c2775
Erik M Baxstrom <baxstrem@gmail.com>
2022-10-13 09:50:43 -0700

    add dummy comment and functioning comment form

--- post-detail/post-detail.js
+++ post-detail/post-detail.js
@@ -12,14 +12,31 @@ const postContent = document.getElementById('post-content');
 const commentList = document.getElementById('comment-list');
 const addCommentForm = document.getElementById('add-comment-form');
 
-<div>
-    <form id="add-comment-form">
-        <button>Comment</button>
-    </form>
-</div>;
-
 /* State */
+let post = null;
+// todo: delete this
+post = {
+    id: 12,
+    text: 'blah',
+};
+let error = null;
 
 /* Events */
+addCommentForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const formData = new FormData(addCommentForm);
+    const newComment = {
+        comment: formData.get('comment'),
+        post_id: post.id,
+    };
+
+    const response = await createComment(newComment);
+    error = response.error;
+    if (error) {
+        alert(error.message);
+    } else {
+        addCommentForm.reset();
+    }
+});
 
 /* Display Functions */
